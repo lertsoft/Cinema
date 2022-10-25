ecb5a2ffb50149338b013cbfedc93777dd33c173
Ronny Coste <ronny21.97@gmail.com>
2022-10-24 22:34:20 -0400

    Tensorflow tests

--- src/assets/scripts/components/Webcam.svelte
+++ src/assets/scripts/components/Webcam.svelte
@@ -1,14 +1,13 @@
 <script>
-// @ts-nocheck
 
-    // @ts-ignore
     import { createEventDispatcher } from 'svelte';
   
-    // @ts-ignore
     import { onMount } from 'svelte';
-    // @ts-ignore
     import { raf, body } from '../modules/utils.js';
     import { loadingChannel, LOADING_STATE } from '../modules/tv.js';
+    // import '@tensorflow/tfjs-core';
+    // import '@tensorflow/tfjs-backend-webgl';
+    // import * as facemesh from "@tensorflow-models/face-landmarks-detection";
   
     const dispatch = createEventDispatcher();
   
@@ -20,7 +19,88 @@
     let elapsedTime;
     let formattedTime;
     let counterRequest;
+
+    // const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+    // const detectorConfig = {
+    //   runtime: 'tfjs',
+    // };
+    // detector = await faceLandmarksDetection.createDetector(model, detectorConfig);
   
+    // const runFacemesh = async () => {
+    //   const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh)
+    //   setInterval(() => {
+    //     detect(net)
+    //   }, 100 )
+    // }
+
+//     async function setupCamera() {
+//   // Find the <video> element in the webpage, 
+//   // then use the mediaDevices API to request a camera from the user
+//   video = document.getElementById('video');
+//   const stream = await navigator.mediaDevices.getUserMedia({
+//     'audio': false,
+//     'video': {
+//       facingMode: 'user',
+//       width: {ideal:1920},
+//       height: {ideal:1080},
+//     },
+//   });
+//   // Assign our camera to the HTML's video element
+//   video.srcObject = stream;
+
+//   return new Promise((resolve) => {
+//     video.onloadedmetadata = () => {
+//       resolve(video);
+//     };
+//   });
+// }
+
+// async function drawVideo(){
+//     // Draw the video stream into our screen
+//     ctx.drawImage(video, 0, 0);
+//     // Call self again
+//     requestAnimationFrame(drawVideo);
+// }
+
+//     // Set up variables to draw on the canvas
+//     var canvas;
+//     var ctx;
+//     const fmesh = facemesh;
+//     async function main() {
+//       await fmesh();
+//         // Set up front-facing camera
+//         await setupCamera();
+//         videoWidth = video.videoWidth;
+//         videoHeight = video.videoHeight;
+//         video.play()
+
+//         // Set up the HTML Canvas to draw the video feed onto
+//         canvas = document.getElementById('facecanvas');
+//         canvas.width = videoWidth;
+//         canvas.height = videoHeight;
+//         ctx = canvas.getContext('2d');
+      
+//         // Start the video->canvas drawing loop
+//         drawVideo();
+
+      
+//         runFacemesh();
+//     }
+
+//     var curFaces = [];
+//     async function runFacemesh() {
+//         // Call face Mesh on our video stream
+//         const facepred = await fmesh.estimateFaces(video);
+        
+//         // If we find a face, export it to a global variable so we can access it elsewhere
+//         if (facepred.length > 0) { 
+//           curFaces = facepred;
+//         }
+//         // Call itself again
+//         requestAnimationFrame(runFacemesh);
+//     };
+
+    
     async function initStream() {
       stream = await window.navigator.mediaDevices
         .getUserMedia({
@@ -67,7 +147,8 @@
         counterRequest = raf(loop);
       });
     }
-  
+    
+    // Fake Recorder timer
     $: {
       // @ts-ignore
       const milliseconds = parseInt((elapsedTime % 1000) / 100);
@@ -76,7 +157,7 @@
       const hours = padNumber(Math.floor((elapsedTime / (1000 * 60 * 60)) % 24));
   
       formattedTime = `${hours}:${minutes}:${seconds}.${milliseconds}`;
-    }
+    } 
   
     onMount(() => {
       initStream();
@@ -90,6 +171,8 @@
         }
       };
     });
+    // main();
+    // runFacemesh();
   </script>
   
   <style lang="postcss">
@@ -132,7 +215,7 @@
   <div
     class:visually-hidden={!isReady || $loadingChannel === LOADING_STATE.Loading}>
     <video bind:this={video} class="tv-video" channel="camera" autoplay />
-  
+    <canvas id="facecanvas"></canvas>  
     <div class="rec-wrapper big-text glitchy-text">
       <div class="rec">REC <span /></div>
       <div class="counter">{formattedTime}</div>
